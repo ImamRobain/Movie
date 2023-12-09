2297a11f381b3cf1754942ad99ef550c861542a1
Imam Robain <imamrobain16@gmail.com>
2023-12-09 22:41:42 +0700

    initial commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: '58b575e9766db246680aa84363fc8627',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
