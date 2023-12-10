3bc29cf10a77e94bb9a195da19cca60b6cd12f15
Imam Robain <imamrobain16@gmail.com>
2023-12-11 00:01:48 +0700

    Update webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,65 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+// // const { GenerateSW } = require('workbox-webpack-plugin');
+// const { InjectManifest } = require('workbox-webpack-plugin');
+// const path = require('path');
+
+// module.exports = {
+//   entry: {
+//     app: path.resolve(__dirname, 'src/scripts/index.js'),
+//     sw: path.resolve(__dirname, 'src/scripts/sw.js'),
+//   },
+//   output: {
+//     filename: '[name].bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     clean: true,
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: [
+//           {
+//             loader: 'style-loader',
+//           },
+//           {
+//             loader: 'css-loader',
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new InjectManifest({
+//       swSrc: path.resolve(__dirname, 'src/scripts/sw.js'),
+//       swDest: './sw.js',
+//     }),
+//     // new GenerateSW({
+//     //   swDest: './sw.budle.js',
+//     // }),
+//     new HtmlWebpackPlugin({
+//       filename: 'index.html',
+//       template: path.resolve(__dirname, 'src/templates/index.html'),
+//     }),
+//     new CopyWebpackPlugin({
+//       patterns: [
+//         {
+//           from: path.resolve(__dirname, 'src/public/'),
+//           to: path.resolve(__dirname, 'dist/'),
+//         },
+//       ],
+//     }),
+//   ],
+// };
+
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-// const { GenerateSW } = require('workbox-webpack-plugin');
-const { InjectManifest } = require('workbox-webpack-plugin');
-const path = require('path');
+// const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
-    sw: path.resolve(__dirname, 'src/scripts/sw.js'),
   },
   output: {
     filename: '[name].bundle.js',
@@ -30,12 +82,9 @@ module.exports = {
     ],
   },
   plugins: [
-    new InjectManifest({
-      swSrc: path.resolve(__dirname, 'src/scripts/sw.js'),
-      swDest: './sw.js',
-    }),
-    // new GenerateSW({
-    //   swDest: './sw.budle.js',
+    // new InjectManifest({
+    //   swSrc: path.resolve(__dirname, 'src/scripts/sw.js'),
+    //   swDest: './sw.js',
     // }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
@@ -46,6 +95,9 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+          globOptions: {
+            ignore: ['**/images/**'],
+          },
         },
       ],
     }),
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,31 @@
+// const { merge } = require('webpack-merge');
+// const common = require('./webpack.common');
+
+// module.exports = merge(common, {
+//   mode: 'production',
+//   devtool: 'source-map',
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: [
+//           {
+//             loader: 'babel-loader',
+//             options: {
+//               presets: ['@babel/preset-env'],
+//             },
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// });
+
+const path = require('path');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
@@ -20,4 +48,50 @@ module.exports = merge(common, {
       },
     ],
   },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
+    new InjectManifest({
+      swSrc: path.resolve(__dirname, 'src/scripts/service-worker.js'),
+      swDest: './serviceWorker.js',
+      maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+    }),
+  ],
+
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [['mozjpeg', { quality: 15, progressive: true }]],
+          },
+        },
+      }),
+    ],
+  },
+
 });
